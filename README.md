45a5002405348516ab4de82f834af336c7f4b9fa
Michael Robinson <kb1dds@gmail.com>
2019-10-20 22:43:11 -0400

    Added SD card interface

--- README.md
+++ README.md
@@ -5,5 +5,6 @@ A small box for improving Morse code copy under difficult conditions, or so I ho
 
 1. Speaker on digital pin D13.  I put a variable resistor in series to control the volume.
 2. Potentiometer on Analog pin A0 for controlling the code speed.
+3. If an SD card containing the file "WORDS.TXT" is desired, than connect SPI bus as indicated in the header comment
 
-I built this using an Arduino Nano in a small plastic box, in which the power is a 9V battery with a small switch to deactivate.  It sends random length strings of between 3 and 5 random characters with a longish pause between them.  It's quite simplistic. 
+I built this using an Arduino Nano in a small plastic box, in which the power is a 9V battery with a small switch to deactivate.  If no SD card with the file "WORDS.TXT" is present, it sends random length strings of between 3 and 5 random characters with a longish pause between them.  It's quite simplistic. 
--- morse_trainer.ino
+++ morse_trainer.ino
@@ -1,17 +1,29 @@
 // Morse code random blocks trainer
+// If an SD card is present with a text file "WORDS.TXT" containing one word per line (all upper case text), it send random words instead of random blocks
 // 
 // Assumptions:
-//  Speaker on D13
-//  Speed potentiometer on A0
+//  Speaker : D5
+//  Speed potentiometer : A0
+// If desired, an SD card attached to SPI bus as follows:
+//  MOSI :  D11
+//  MISO :  D12
+//  CLK  :  D13
+//  CS   :  D4
 //
 // Copyright (c) 2019 Michael Robinson
 
-#define SPK_PIN 13   // Digital pin for speaker
-#define SPD_PIN 0    // Analog pin for potentiometer
-#define TONE 700 // Tone of the signal in Hz
+#include <SPI.h>
+#include <SD.h>
 
-#define BLK_MIN 3
-#define BLK_MAX 5
+#define SPK_PIN 5   // Digital pin for speaker
+#define SPD_PIN 0   // Analog pin for potentiometer
+#define SD_CS_PIN 4 // Chip select for SD card
+#define TONE 700    // Tone of the signal in Hz
+
+#define BLK_MIN 3  // Minimum random block length
+#define BLK_MAX 5  // Maximum random block length
+
+#define WORD_MAX 15 // Word buffer length, must be longer than BLX_MAX
 
 const char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890./=";
 #define ALPHABET_LEN 39
@@ -19,6 +31,10 @@ const char *code[] = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "
   "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "-----", ".-.-.-", "-..-.", "-...-"};
 
 int spd = 125; // Current length of a dot in ms
+
+// SD card details for "words.txt"
+unsigned int total_words=0; // Number of words in the SD card file "words.txt".  If zero, then just send random strings of characters
+File fp;
 
 void string_send( char *string )
 {
@@ -78,19 +94,56 @@ void setup()
   pinMode( SPK_PIN, OUTPUT );
   randomSeed(analogRead(1));
   //string_send("HELLO WORLD.");
+  Serial.begin(9600);
+
+  // Set up the words.txt file, if it exists
+  if (!SD.begin(SD_CS_PIN)) {
+    string_send("NO SD.");
+    Serial.println("NO SD.");
+  }
+  else {
+    fp = SD.open("words.txt");
+    if( fp ){
+      // Successfully opened the dictionary.  Count words in the file
+      while( fp.available() ){
+        if(fp.read() == '\n' )
+          total_words ++;
+      }
+    }
+    else{
+      string_send("NO WORDS.TXT.");
+      Serial.println("NO WORDS.TXT.");
+    }
+  }
 }
 
 void loop()
 {
-  char str[BLK_MAX];
-  int i;
-  
-  for( i = 0; i < BLK_MAX; i ++ ) 
-    str[i] = ' ';
+  char str[WORD_MAX], ch;
+  int i, wordnum;
 
-  for( i = 0; i < random(BLK_MIN, BLK_MAX); i ++ )
-    str[i] = alphabet[random(0,ALPHABET_LEN)];
+  if( total_words == 0 ){  // No words file
+    for( i = 0; i < BLK_MAX; i ++ ) 
+      str[i] = ' ';
 
-  string_send(str);
-  cw_send(";;;;;");
+    for( i = 0; i < random(BLK_MIN, BLK_MAX); i ++ )
+      str[i] = alphabet[random(0,ALPHABET_LEN)];
+
+    string_send(str);
+    Serial.println(str);
+    cw_send(";;;;;");
+  }
+  else {  // Use words file
+    fp.seek(0);
+    wordnum = random(0,total_words-1);
+    for ( i = 0; i < wordnum; i ++ ){ 
+      while( fp.read() != '\n' );
+    }
+    for( i = 0; (i < WORD_MAX) && ((ch=fp.read()) != '\n'); i ++ )
+      str[i]=ch;
+    str[i]=0;  
+    string_send(str);
+    Serial.println(str);
+    cw_send(";;;;;");
+  }
 }
